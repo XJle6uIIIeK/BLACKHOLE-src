73f32e037cb9ff5234894e7bd036de8b8a69b80a
XJle6uIIIeK <82363409+XJle6uIIIeK@users.noreply.github.com>
2025-11-17 00:25:33 +0300

    Delete Medusa.uno/Hacks directory

--- Medusa.uno/Hacks/Fakelag.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Fakelag
-{
-    void run(bool& sendPacket) noexcept;
-    inline int latest_choked_packets{};
-}
--- Medusa.uno/Hacks/Ragebot_v2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../SDK/UserCmd.h"
-
-namespace RagebotV2 {
-    
-    // Main ragebot execution
-    void run(UserCmd* cmd) noexcept;
-}
